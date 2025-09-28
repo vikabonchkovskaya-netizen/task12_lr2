2f311d36996eb8e64aaf964e1965c497901d3dbc
Vika <vikabonchkovskaya@gmail.com>
2025-09-28 14:14:06 +0300

    .

--- task12_lr2.cpp
+++ task12_lr2.cpp
@@ -13,44 +13,60 @@ double cor(double x) {
     return mid;
 }
 
-// Решение биквадратного уравнения (1)
-void solveType1(double a, double b, double c) {
-    std::cout << "\nТип 1: Биквадратное уравнение\n";
+int kvad(double a, double b, double c, double& x1, double& x2) {
+    double D = b * b - 4 * a * c;
+    if (D < 0) return 0;
+    else if (D == 0) {
+        x1 = -b / (2 * a);
+        return 1;
+    }
+    else {
+        x1 = (-b + cor(D)) / (2 * a);
+        x2 = (-b - cor(D)) / (2 * a);
+        return 2;
+    }
+}
+
+void solveType2(double a, double b, double c) {
+    std::cout << "Тип 2: Симметричное уравнение\n";
     if (a == 0) {
-        std::cout << "Это не биквадратное уравнение.\n";
+        std::cout << "Это не симметричное уравнение.\n";
         return;
     }
 
-    // x^2 = t
-    double D = b * b - 4 * a * c;
-    if (D < 0) {
+    // az^2 + bz + (c-2a) = 0
+    double z1, z2;
+    int zRoots = kvad(a, b, c - 2 * a, z1, z2);
+
+    if (zRoots == 0) {
         std::cout << "Нет действительных корней.\n";
         return;
     }
-    double t1 = (-b + cor(D)) / (2 * a);
-    double t2 = (-b - cor(D)) / (2 * a);
 
+    double x1, x2, x3, x4;
     int count = 0;
-    if (t1 >= 0) {
-        double x1 = cor(t1);
-        double x2 = -x1;
-        std::cout << "x = " << x1 << " " << x2 << " ";
-        count += 2;
+
+    if (zRoots >= 1) {
+        int roots1 = kvad(1, -z1, 1, x1, x2);
+        if (roots1 == 1) std::cout << "x = " << x1 << std::endl;
+        if (roots1 == 2) std::cout << "x = " << x1 << ", " << x2 << std::endl;
+        count += roots1;
     }
-    if (t2 >= 0 && t2 != t1) {
-        double x3 = cor(t2);
-        double x4 = -x3;
-        std::cout << x3 << " " << x4 << "\n";
-        count += 2;
+    if (zRoots == 2) {
+        int roots2 = kvad(1, -z2, 1, x3, x4);
+        if (roots2 == 1) std::cout << "x = " << x3 << std::endl;
+        if (roots2 == 2) std::cout << "x = " << x3 << ", " << x4 << std::endl;
+        count += roots2;
     }
+
     if (count == 0) std::cout << "Нет действительных корней.\n";
 }
+
 
 int main() {
     setlocale(LC_ALL, "RU");
     double a, b, c;
     std::cout << "Введите a, b, c: ";
     std::cin >> a >> b >> c;
-    solveType1(a, b, c);
-    return 0;
+    solveType2(a, b, c);
 }
